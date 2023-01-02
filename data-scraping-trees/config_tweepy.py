d10754df0e6f6932646b7f553dec2bee8ec8f5ca
Kerria Pang-Naylor <kerria.lyona11235@gmail.com>
2023-01-02 03:41:02 -0800

    edited config

--- data-scraping-trees/config_tweepy.py
+++ data-scraping-trees/config_tweepy.py
@@ -3,4 +3,4 @@ Description:
 This file contains the keys and tokens needed to initialize the tweepy client that accesses the twitter API. This file must be imported for any data scraping script.
 (Author: Kerria Pang-Naylor)
 """
-
+# removed
